fdf2a8cb5bfc384ba2d5855687cd91ed10b351ad
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-24 22:58:59 +0100

    Creates Hero Section comp

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Image from "next/image";
+import CountUpNumber from "../CountUpNumber/CountUpNumber";
+
+const HeroSection = () => {
+  return (
+    <section className="flex px-4 items-center gap-12 container mx-auto">
+      <div className="py-10 h-full">
+        <h1 className="font-heading mb-6">Explore Our Fantastic Hotels</h1>
+        <p className="text-[#4a4a4a] dark:text-[#ffffffea] mb-12 max-w-lg">
+          From our budget-friendly basic rooms, to our lush luxury rooms and
+          impressive suites.
+        </p>
+        <button className="btn-primary">Get Started</button>
+
+        <div className="flex justify-between mt-12">
+          <div className="flex gap-3 flex-col items-center justify-center">
+            <p className="text-xs lg:text-xl text-center">Basic Room</p>
+            <CountUpNumber duration={3000} endValue={200} />
+          </div>
+          <div className="flex gap-3 flex-col items-center justify-center">
+            <p className="text-xs lg:text-xl text-center">Luxury Room</p>
+            <CountUpNumber duration={3000} endValue={230} />
+          </div>
+          <div className="flex gap-3 flex-col items-center justify-center">
+            <p className="text-xs lg:text-xl text-center">Suite Room</p>
+            <CountUpNumber duration={3000} endValue={160} />
+          </div>
+        </div>
+      </div>
+
+      <div className="md:grid hidden gap-8 grid-cols-1">
+        <div className="rounded-2xl overflow-hidden h-48">
+          <Image
+            src="/images/hero-1.jpeg"
+            alt="hero-1"
+            width={300}
+            height={300}
+            className="hover:scale-125 transition-all duration-700 object-cover w-full h-full"
+          />
+        </div>
+
+        <div className="grid grid-cols-2 gap-8 h-48">
+          <div className="rounded-2xl overflow-hidden">
+            <Image
+              src="/images/hero-2.jpeg"
+              alt="hero-2"
+              width={300}
+              height={300}
+              className="hover:scale-125 transition-all duration-700 object-cover w-full h-full"
+            />
+          </div>
+          <div className="rounded-2xl overflow-hidden">
+            <Image
+              src="/images/hero-3.jpeg"
+              alt="hero-3"
+              width={300}
+              height={300}
+              className="hover:scale-125 transition-all duration-700 object-cover w-full h-full"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
