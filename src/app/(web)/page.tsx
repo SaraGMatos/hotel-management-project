0026758a0d6707f312b53f5dfc6e7c45479cd8b8
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-25 12:28:33 +0100

    Creates basic search comps

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = () => {
   return (
     <>
       <HeroSection></HeroSection>
+      <PageSearch />
+      {/* Featured room  */}
+      {/* Gallery */}
+      {/* News Letter */}
     </>
   );
 };
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,27 @@
+const Search = () => {
+  return (
+    <section className="bg-tertiary-light px-4 py-6 rounded-lg">
+      <div className="container mx-auto flex gap-4 flex-wrap justify-between items-center">
+        <div className="w-full.md:1/3.lg:w-auto.mb-4.md:mb-0">
+          <label className="block text-sm font-medium mb-2 text-black">
+            Room Type
+          </label>
+          <div className="relative">
+            <select
+              // value={roomTypeFilter}
+              // onChange={handleRoomTypeChange}
+              className="w-full px-4 py-2 capitalize rounded leading-tight dark:bg-black focus:outline-none"
+            >
+              <option value="All">All</option>
+              <option value="Basic">Basic</option>
+              <option value="Luxury">Luxury</option>
+              <option value="Suite">Suite</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Search;
