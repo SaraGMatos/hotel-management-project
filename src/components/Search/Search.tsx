73a81b32cb410b0adde3458ebdb6a373954b1770
SaraGarciaM <saragarciamatos@gmail.com>
2024-08-20 19:21:55 +0100

    Adds room type and search inputs

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,46 @@
-const Search = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ChangeEvent } from "react";
+
+interface Props {
+  roomTypeFilter: string;
+  searchQuery: string;
+  setRoomTypeFilter: (value: string) => void;
+  setSearchQuery: (value: string) => void;
+}
+
+const Search = ({
+  roomTypeFilter,
+  searchQuery,
+  setRoomTypeFilter,
+  setSearchQuery,
+}: Props) => {
+  const router = useRouter();
+
+  const handleRoomTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setRoomTypeFilter(event.target.value);
+  };
+
+  const handleSetQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleFilterClick = () => {
+    router.push(`/rooms?roomType=${roomTypeFilter}&searchQuery=${searchQuery}`);
+  };
+
   return (
     <section className="bg-tertiary-light px-4 py-6 rounded-lg">
       <div className="container mx-auto flex gap-4 flex-wrap justify-between items-center">
-        <div className="w-full.md:1/3.lg:w-auto.mb-4.md:mb-0">
+        <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
           <label className="block text-sm font-medium mb-2 text-black">
             Room Type
           </label>
           <div className="relative">
             <select
-              // value={roomTypeFilter}
-              // onChange={handleRoomTypeChange}
+              value={roomTypeFilter}
+              onChange={handleRoomTypeChange}
               className="w-full px-4 py-2 capitalize rounded leading-tight dark:bg-black focus:outline-none"
             >
               <option value="All">All</option>
@@ -19,6 +50,28 @@ const Search = () => {
             </select>
           </div>
         </div>
+
+        <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
+          <label className="block text-sm font-medium mb-2 text-black">
+            Search
+          </label>
+          <input
+            type="search"
+            id="search"
+            placeholder="Search..."
+            className="w-full px-4 py-3 rounded leading-tight dark:bg-black focus:outline-none dark:placeholder:text-white"
+            value={searchQuery}
+            onChange={handleSetQueryChange}
+          />
+        </div>
+
+        <button
+          className="btn-primary"
+          type="button"
+          onClick={handleFilterClick}
+        >
+          Search
+        </button>
       </div>
     </section>
   );
