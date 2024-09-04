eea990611ddaf20196ac3119ccf684ba89974363
SaraGarciaM <saragarciamatos@gmail.com>
2024-09-04 09:59:25 +0100

    Fixes bug in feature room

--- src/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/components/FeaturedRoom/FeaturedRoom.tsx
@@ -39,27 +39,23 @@ const FeaturedRoom = ({ featuredRoom }: Props) => {
       <div className="md:py-10 md:w-1/2 text-left">
         <h3 className="font-heading mb-12 text-center">Featured Room</h3>
 
-        <p className="font-normal max-w-md">{featuredRoom.description}</p>
+        <p className="font-normal text-justify lg:text-center mx-4">
+          {featuredRoom.description}
+        </p>
 
         <div className="flex flex-col md:flex-row md:items-end justify-between mt-5">
-          <div className="flex mb-3 md:mb-0">
+          <div className="flex mb-3 md:mb-0 justify-center">
             <div className="flex gap-3 flex-col items-center justify-center mr-4">
               <p className="text-xs lg:text-xl text-center">Start From</p>
               <p className="md:font-bold flex font-medium text-lg xl:text-5xl">
                 £ {featuredRoom.price}
               </p>
             </div>
-            <div className="flex gap-3 flex-col items-center justify-center mr-4">
-              <p className="text-xs lg:text-xl text-center">Discount</p>
-              <p className="md:font-bold flex font-medium text-lg xl:text-5xl">
-                £ {featuredRoom.discount}
-              </p>
-            </div>
           </div>
 
           <Link
             href={`/rooms/${featuredRoom.slug.current}`}
-            className="border h-fit text-center border-tertiary-dark text-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl"
+            className="border h-fit text-center border-tertiary-dark text-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl mx-4"
           >
             More Details
           </Link>
